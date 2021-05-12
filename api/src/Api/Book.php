6886037b39cb6b147c78b1a63d02463629e4ade8
bishoy <bishoybishoo2012@gmail.com>
2021-05-12 16:52:55 +0200

    get book by id

--- api/src/Api/Book.php
+++ api/src/Api/Book.php
@@ -47,4 +47,19 @@ class Book
 
         return json_encode(["error" => 500, "message" => $mysqli->error]);
     }
+
+    public static function getbook($book_id)
+    {
+        require 'db/dbconnect.php';
+
+        $sql = "SELECT * FROM books WHERE books.id = '$book_id'";
+
+        $result = $mysqli->query($sql);
+        $books = array();
+        if ($result->num_rows > 0) {
+            return html_entity_decode(json_encode(["success" => true, "bookdata" => $row]));
+        }
+
+        return json_encode(["error" => 404, "message" => "book not found"]);
+    }
 }
